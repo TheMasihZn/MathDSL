e05c01955c3011b281870b4f780c4aa1957d823e
GitHub Action <action@github.com>
2025-05-14 23:48:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/themasihzn/mathdsl/services/MyProjectService.kt
+++ src/main/kotlin/com/github/themasihzn/mathdsl/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.themasihzn.mathdsl.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.themasihzn.mathdsl.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
